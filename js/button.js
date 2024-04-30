b1f2e968364d6e33608735fcc9a879638849183b
elenagalletta <elenabeatricegalletta@abacatania.it>
2024-04-30 13:54:45 +0200

    Add pages + button

---
+++ js/button.js
@@ -0,0 +1,9 @@
+function changeImage() {
+    var img = document.getElementById("button");
+    img.src = "/img/button-dark.png";
+}
+
+function restoreImage() {
+    var img = document.getElementById("button");
+    img.src = "/img/button-light.png";
+}
